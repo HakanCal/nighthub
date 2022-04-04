191abbdb865ea7d703d0b71aa59b7d0a98285bf4
HakanCal <hakancaliskan1@live.de>
2022-04-04 16:45:57 +0200

    radar page

--- lib/src/radar/radar.dart
+++ lib/src/radar/radar.dart
@@ -3,33 +3,45 @@ import 'package:nighthub/src/radar/radarlist.dart';
 
 class Radar extends StatefulWidget {
   const Radar({Key? key}) : super(key: key);
-
   @override
   State<StatefulWidget> createState() => _Radar();
 }
 
 class _Radar extends State<Radar> {
+  String _sortMethod = 'distance';
 
-  String sortMethod = 'distance';
-//TODO better ui
   @override
   Widget build(BuildContext context) {
     return Column(
         children:[
           // Menu for changing List
-          Row(children:[TextButton(child: Text('Sort by $sortMethod'), onPressed: () => {setState((){sortMethod = 'rating';})})]),
-          // Show a scrollabe List of businesses near
-          const Scrollbar(
-            isAlwaysShown: true,
+          Container(
+              margin: const EdgeInsets.symmetric(horizontal: 2),
+              child: Row(
+                  children:[
+                    const Icon(Icons.list_rounded,color: Colors.orange ),
+                    TextButton(
+                        child: Text('Sort by ${_sortMethod}'),
+                        onPressed: () => setState(() {
+                          _sortMethod = _sortMethod == 'distance' ? 'rating' : 'distance';
+                        })
+                    )
+                  ]
+              )
+          ),
+          // List with Shops
+          Expanded(child: Scrollbar(
+            isAlwaysShown: false,
             //always show scrollbar
             thickness: 10,
             //width of scrollbar
-            radius: Radius.circular(20),
+            radius: const Radius.circular(20),
             //corner radius of scrollbar
             scrollbarOrientation: ScrollbarOrientation.left,
-            child: RadarList()
-          )
+            child: RadarList(sortMethod: _sortMethod)
+          ))
         ]
     );
   }
+
 }
--- lib/src/radar/radarlist.dart
+++ lib/src/radar/radarlist.dart
@@ -1,60 +1,122 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:nighthub/src/radar/radaritem.dart';
 
+
 class RadarList extends StatefulWidget{
-  const RadarList({Key? key}) : super(key: key);
+  const RadarList({this.sortMethod = 'distance' ,Key? key}) : super(key: key);
+  final String sortMethod;
+
   @override
   State<StatefulWidget> createState() => _RadarList();
 }
 
-class _RadarList extends State<RadarList>{
 
-  //final List<RadarItem> items;
-  static List<Widget> radaritems = [
-    const Text('Item 1'),
-    const Text('Item 2'),
-    const Text('Item 3'),const Text('Item 3'),const Text('Item 3'),const Text('Item 3'),const Text('Item 3'),
-    Container(
-      height: 120,
-        decoration: BoxDecoration(
-          color: Colors.black,
-          border: Border.all(color: Colors.deepOrange, width: 10),
-          borderRadius: BorderRadius.circular(20)
-        ),
-        child: Row(children: [
-          Expanded(child: FittedBox(fit: BoxFit.contain,child:Image.network('https://logos-download.com/wp-content/uploads/2016/05/Coffeeshop_Company_logo_logotype.png'))),
-          Expanded(child: Column(children: [Text('Shop Name Example', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold)),SizedBox(height: 10),Text('kat')])),
-          Expanded(child: Column(children: [Text('Rating: 4/5'),SizedBox(height: 10),Text('Distance: 20km')]))
-        ])
-    )
-  ];
-  //final Position position;
-  //TODO get shops near me
-  // loads in businesses near me
-  Future<void> getItems() async {
+
+class _RadarList extends State<RadarList>{
+  Geoflutterfire geo = Geoflutterfire();
+  Position _userPos = const Position(latitude: 48.783333, longitude: 9.183333, accuracy: 1.0, altitude: 1, speedAccuracy: 1.0, timestamp: null, speed: 1.0, heading: 1.0);
+  List<RadarItem> _radaritems = [];
+  sortBy(){
+      switch(widget.sortMethod){
+        case 'distance':
+          _radaritems.sort((a, b) => a.distance.compareTo(b.distance));
+          break;
+        case 'rating':
+          _radaritems.sort((a, b) => b.rating.compareTo(a.rating));
+          break;
+      }
+  }
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      await Geolocator.requestPermission();
+      //return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.'
+      );
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+  static Image logo = Image.network('https://logos-download.com/wp-content/uploads/2016/05/Coffeeshop_Company_logo_logotype.png');
+
+  Future<void> _getItems() async {
     print('getting new items...');
-    radaritems.add(const Text('new Item'));
-    setState(() {});
-    // request new shops from db
-    // shops.forEach((shop) => {items.add(RadarItem(name: shop.name, logo: shop.logo ... ))})
+    await Firebase.initializeApp();
+    _userPos = await _determinePosition();
+    // Geo query nearby items 25km
+    GeoFirePoint center = geo.point(latitude: _userPos.latitude, longitude: _userPos.longitude);
+    Query<Map<String,dynamic>> collectionRef = FirebaseFirestore.instance.collection('entity_accounts');
+    Stream<List<DistanceDocSnapshot>> stream = geo.collection(collectionRef: collectionRef)
+        .withinWithDistance(center: center, radius: 25, field: 'point');
+
+    _radaritems = [];
+    stream.listen((List<DistanceDocSnapshot> shopDocuments) => {
+       shopDocuments.forEach((shop) {
+         setState(() {
+              _radaritems.add(
+                  RadarItem(
+                    name: shop.documentSnapshot['entityName'],
+                    logo: logo,
+                    address: shop.documentSnapshot['address'],
+                    distance: shop.kmDistance,
+                    categories: shop.documentSnapshot['interests'],
+                    rating: 1
+                  )
+              );
+          });
+      })
+
+    });
+    sortBy();
+    //setState(() {_radaritems = _radaritems;});
+  }
+  @override
+  void initState() {
+    _getItems().then((value) {
+      print('trying to sort');
+      sortBy();
+    });
+    super.initState();
+
   }
 
   @override
   Widget build(BuildContext context) {
+
+    print(_radaritems);
     return RefreshIndicator(
         child: ListView.separated(
             itemBuilder: (BuildContext context, int index) {
-              Widget radaritem = radaritems[index];
+              RadarItem radaritem = _radaritems[index];
               return Center(child: radaritem);
             },
             scrollDirection: Axis.vertical,
-            shrinkWrap: true, //TODO make it scrollable
-            itemCount: radaritems.length,
+            shrinkWrap: true,
+            itemCount: _radaritems.length,
             separatorBuilder: (BuildContext context, int index) {
               return const SizedBox(height: 10);
             }
         ),
-        onRefresh: getItems // refreshes list content on swiping down
+        onRefresh: _getItems // refreshes list content on swiping down
     );
   }
 }
